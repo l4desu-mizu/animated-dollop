ed6b182d7274dba16e4c7611acdbeee8bb8931f3
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2024-02-03 18:48:43 +0100

    somewhat working example for frontend/angular stuff
    * loading the actual wasm implementation is still an issue. I'm not sure
      how to tell angular to bundle it with the rest of the code and also
      how the dev server handles loading the implementation (currently it
      tries to load from fs which fails)

--- my_ui/src/app/app.component.ts
+++ my_ui/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MyType} from 'multilang_playground';
+import init, {MyType} from 'multilang_playground';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +12,7 @@ import {MyType} from 'multilang_playground';
 export class AppComponent implements OnInit {
   title = 'my_ui';
   async ngOnInit(){
+    await init();
     const a = new MyType(5, "test");
     console.log(a.do_stuff());
   }
--- serve.py
+++ serve.py
@@ -2,21 +2,11 @@ from pprint import pprint
 
 import uvicorn
 from fastapi import FastAPI
-from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
+
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="./my_ui/dist/my_ui/browser"))
-
-templates = Jinja2Templates(directory="./my_ui/dist/my_ui/browser")
-
-
-@app.get("/")
-def get(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="index.html"
-    )
+app.mount("/", StaticFiles(directory="./my_ui/dist/my_ui/"))
 
 
 if __name__ == '__main__':
